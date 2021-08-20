c08eafdcd31aaff2e5f048c20ac8553045ad5c4c
Michael Partridge <mcp292@nau.edu>
2021-08-20 11:24:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Quick script to test different approaches to the problem of detecting
+the last item in a list, prompted by this [SO post](https://
+stackoverflow.com/questions/39808908/detect-if-item-is-the-last-in-a-
+list).
+
+Results on my machine:
+```txt
+check_last_num():            3.9227688719984144
+check_last_num_ind():        5.9376225670021086
+check_last_num_ind_acc():    6.796169109999028
+skip_last_num():             4.144758571997954
+```
+
+I figure python must be optimizing the for each loop.
---
+++ find_last.py
@@ -0,0 +1,73 @@
+# Results: Seems that check last goes quicker for less iterations. I assume this
+# is the diff of for each loop. Storing list item as variable is faster than
+# repeated access.
+
+import random
+from timeit import timeit
+
+
+def check_last_num(nums):
+    nums_sum = 0
+    for num in nums:
+        if num is not nums[-1]:
+           nums_sum += num
+
+
+def check_last_num_ind(nums):
+    nums_sum = 0
+    for ind in range(len(nums)):
+        num = nums[ind]
+        if num is not nums[-1]:
+           nums_sum += num
+
+
+def check_last_num_ind_acc(nums):
+    nums_sum = 0
+    for ind in range(len(nums)):
+        if nums[ind] is not nums[-1]:
+           nums_sum += nums[ind]
+
+
+def skip_last_num(nums):
+    nums_sum = 0
+    for ind in range(len(nums) - 1):
+        nums_sum += nums[ind]
+
+
+def get_rand_list():
+    MIN = 0
+    MAX = int(1e4)
+    SIZE = int(1e4)
+
+    random.seed()
+
+    rand_list = []
+    for iter in range(SIZE):
+        rand_list.append(random.randint(MIN, MAX))
+
+    return rand_list
+
+
+def main():
+    ITERATIONS = int(1e4)
+
+    print("check_last_num():\t\t\t",
+          timeit("check_last_num(rand_list)", setup="from __main__ import "
+                 "check_last_num, get_rand_list\nrand_list = get_rand_list()",
+                 number=ITERATIONS))
+    print("check_last_num_ind():\t\t",
+          timeit("check_last_num_ind(rand_list)", setup="from __main__ import "
+                 "check_last_num_ind, get_rand_list\nrand_list = get_rand_list()",
+                 number=ITERATIONS))
+    print("check_last_num_ind_acc():\t",
+          timeit("check_last_num_ind_acc(rand_list)", setup="from __main__ import "
+                 "check_last_num_ind_acc, get_rand_list\nrand_list = get_rand_list()",
+                 number=ITERATIONS))
+    print("skip_last_num():\t\t\t",
+          timeit("skip_last_num(rand_list)", setup="from __main__ import "
+                 "skip_last_num, get_rand_list\nrand_list = get_rand_list()",
+                 number=ITERATIONS))
+
+
+if __name__ == "__main__":
+    main()
